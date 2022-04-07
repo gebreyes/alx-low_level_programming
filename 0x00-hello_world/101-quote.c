4451191620504270390d02eb4be3219ddc9604b0
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-07 19:14:04 +0300

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
