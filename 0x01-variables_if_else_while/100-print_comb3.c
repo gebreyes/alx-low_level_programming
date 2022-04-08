d648dc7649387d2bfbd65a7007300a6691e3116d
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-08 21:34:14 +0300

    combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,16 @@ int main(void)
 {
 int i;
 int j;
-for (i = 0; i < 10; i++)
+for (i = 0; i < 9; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = i; j < 10; j++)
 {
 if (i == j)
 {
 continue;
 }
-putchar(i % 10);
-putchar((j + 1) % 10);
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
 putchar(',');
 putchar(' ');
 }}
