605053e860f6b360714f4796be1f58266d2dceaa
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-08 23:04:57 +0300

    advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,8 +19,10 @@ putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
+{
 continue;
-putchar(',');											
+}
+putchar(',');
 putchar(' ');
 }}
 putchar('\n');
