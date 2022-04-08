df60010e550b3c6a43334fa682562e6861b0f67a
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-08 18:24:39 +0300

    printing in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-int i;
-for (i = 0; i < 16; i++)
-putchar((i) + '0');
-putchar('\n');
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
 
 return (0);
 }
