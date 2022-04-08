49a65bb01418b357d638c92001f247cc841d8f31
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-08 20:55:29 +0300

    combination of two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (i == j)
+{
+continue;
+}
+else
+{
+putchar(i % 10);
+putchar((j % 10) + 1);
+putchar(',');
+}
+}
+}
+putchar('\n');
+}
+
+return (0);
