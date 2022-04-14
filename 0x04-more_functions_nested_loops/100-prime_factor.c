32e8f4b159b22eec83d670c4c0b2b29be19c49e4
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-14 18:09:37 +0300

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-int main()
+
+int main(void)
 {
    long long int n;
    n=612852475143;
    long int div=2, ans = 0, maxFact;
-   while(n!=0) {
-      if(n % div !=0)
-         div = div + 1;
-      else {
-         maxFact = n;
-         n = n / div;
-         if(n == 1) {
-            printf("%d is the largest prime factor !",maxFact);
-            ans = 1;
-            break;
+   while(n!=0) 
+   {
+	   if(n % div !=0)
+		div = div + 1;
+	   else 
+	   {
+		maxFact = n;
+         	n = n / div;
+	if(n == 1) 
+	{
+		printf("%d is the largest prime factor !",maxFact);
+		ans = 1;
+		break;
          }
       }
    }
