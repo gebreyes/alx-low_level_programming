eba94b325bb4e290a99d5e19dbb8a8419460911d
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-21 23:41:42 +0300

    commiting magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,23 @@
-#include "holberton.h"
+#include <stdio.h>
 
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-	int count;
+  int n;
+  int a[5];
+  int *p;
 
-	for (count = 0; count <= )
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
