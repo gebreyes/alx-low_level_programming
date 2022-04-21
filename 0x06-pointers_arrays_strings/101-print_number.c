28f7ff9b7f1fcf1834a19abbb2db96115eef885e
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-04-21 23:21:50 +0300

    main header

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,43 +7,17 @@
 
 void print_number(int n)
 {
-	int power;
-	int neg;
-	int hold;
+	unsigned int num;
 
-	neg = 0;
-	power = 1;
-	hold = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		neg = 1;
+		num = -n;
 	}
+	else
+		num = n;
 
-	while (hold > 9 || hold < -9)
-	{
-		power *= 10;
-		hold /= 10;
-	}
-
-	while (power > 0)
-	{
-		if (power > 9)
-		{
-			if (!neg)
-				_putchar((n / power % 10) + '0');
-			else
-				_putchar((n / power % 10) * -1 + '0');
-
-			power /= 10;
-		}
-		if (power == 1)
-		{
-			if (neg)
-				_putchar((n % 10) * -1 + '0');
-			else
-				_putchar(n % 10 + '0');
-			power = 0;
-		}
-	}
+	if (num / 10)
+		print_number(num / 10);
+	_putchar('0' + num % 10);
 }
