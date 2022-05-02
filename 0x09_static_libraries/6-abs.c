f19128d1dec1597ef3bd48afc9195ebfffbbeaa7
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-05-02 23:51:04 +0300

    creating static libaries task o

---
+++ 0x09_static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "yes.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
