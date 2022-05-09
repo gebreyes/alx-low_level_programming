0546c901a2a031b677c7366dd78ea3a7c2d53c27
Gebreyes Gebeyehu <martaassefa23@gmail.com>
2022-05-09 17:00:13 +0300

    printing absolute path of 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: 1 if an error occurred, 0 otherwise
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
