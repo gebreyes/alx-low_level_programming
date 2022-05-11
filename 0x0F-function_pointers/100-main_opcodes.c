40e33df07f7ca5ceee636c7cd39655ccb90c0a34
Gebreyes Gebeyehu <martaassefa23@gmail.com>
2022-05-11 22:39:41 +0300

    advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,35 +1,37 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - check the code for Holberton School students.
- * @argc: amount of args
- * @argv: arguments
- * Return: 1 if an error occurred, 0 otherwise
- */
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
 int main(int argc, char *argv[])
 {
-	unsigned char *p = (unsigned char *)main;
+	int i, n;
 
-	unsigned int bytes;
-
-	int i;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	bytes = atoi(argv[1]);
-
-	for (i = 0; i < bytes; i++)
-		printf("%02x ", *p++);
-	printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
