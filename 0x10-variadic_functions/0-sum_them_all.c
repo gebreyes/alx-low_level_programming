4e1d95b05c7657a27ec4a3632ef73c95928b0671
Gebreyes Gebeyehu <martaassefa23@gmail.com>
2022-05-12 21:45:00 +0300

    summation of n number

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list x;
 	int sum = 0;
 	unsigned int i;
-	if(n==0)
+	if(n == 0)
 		return (0);
 
 	va_start(x, n);
-	for(i=0; i<n; i++)
-		sum+=va_arg(x, int);
+	for(i = 0; i < n; i++)
+		sum += va_arg(x, int);
 	va_end(x);
 	return (sum);
 }
