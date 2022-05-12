e7d09a7b97e94ac3538703edd4cb11849b2a8baa
Gebreyes Gebeyehu <martaassefa23@gmail.com>
2022-05-12 21:47:59 +0300

    summation of n number

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,5 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * sum_them_all - return the sum of a & b
  * @n: number of args
@@ -13,7 +12,6 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	if(n == 0)
 		return (0);
-
 	va_start(x, n);
 	for(i = 0; i < n; i++)
 		sum += va_arg(x, int);
