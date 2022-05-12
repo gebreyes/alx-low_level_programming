f3775da28216acbe68bae045eb058b811eabf067
Gebreyes Gebeyehu <martaassefa23@gmail.com>
2022-05-12 21:09:26 +0300

    summation of n number

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list x;
+	va_start(x, n);
+	int sum = 0;
+	int i;
+	if(n==0)
+		return (0);
+	for(i=0; i<n;i++)
+	{
+		sum+=va_arg(x, unsigned int);
+	}
+	va_end(x);
+	return (sum);
+}
