6069712f4abc5d4d5fbebb68ed93c7f8e6787299
Gebreyes Gebeyehu <martaassefa23@gmail.com>
2022-05-12 23:47:57 +0300

    printing number

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings
+ * @separator: string separator
+ * @n: number of args
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+
+	unsigned int i;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *c = va_arg(list, char *);
+
+		if (c)
+			printf("%s", c);
+		else
+			printf("(nil)");
+
+		if (i == n - 1)
+			break;
+
+		if (separator)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(list);
+}
