52be3b18a61825f5afd416b06478016d32d56ec5
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-05-19 16:33:46 +0300

    returning number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * list_len - returns the number of elements in list
- * Return: num of elements in list
- * @h: list
- */
+  * list_len - Show the number of elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of elements of a list
+  */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h)
 	{
+		h = h->next;
 		count++;
-		node = node->next;
-		
 	}
+
 	return (count);
 }
