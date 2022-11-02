17497b59018c65e163d85aae52e17397f9e1c32c
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-11-02 11:04:53 +0300

    search algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 
-
 /**
  * print_array - Prints an array of integers
  *
@@ -26,24 +25,21 @@ void print_array(const int *array, int low, int high)
 
 
 /**
- * binary_search - a function that searches for a value in a sorted
+ * __binary_search - a function that searches for a value in a sorted
  *	array of integers using the Binary search algorithm
  * @array: array of integers
- * @size: size of the array
+ * @low: low index
+ * @high: high index
  * @value: the value to search for in the array
  * Return: if the value was found returns its index else returns -1
  */
 
-int binary_search(int *array, size_t size, int value)
+int __binary_search(int *array, int low, int high, int value)
 {
 int mid;
-int low, high;
 
 if (!array)
 	return (-1);
-
-low = 0;
-high = size - 1;
 
 
 while (low <= high)
@@ -59,4 +55,36 @@ while (low <= high)
 }
 
 return (-1);
+}
+
+
+/**
+ * exponential_search -  a function that searches for a value in a
+ * sorted array of integers using the exponential search algorithm
+ * @array: The array of integers
+ * @size: size of the array
+ * @value: value to search for in the array
+ * Return: index of the value
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+int b, x;
+
+if (!array || !size)
+	return (-1);
+
+b = 1;
+
+while (b < (int)size && array[b] <= value)
+{
+	printf("Value checked array[%d] = [%d]\n", b, array[b]);
+	b *= 2;
+}
+
+x = b < (int)size ? b : (int)size - 1;
+
+printf("Value found between indexes [%d] and [%d]\n", b / 2, x);
+
+return (__binary_search(array, b / 2, x, value));
 }
